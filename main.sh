b6de0741234aab1e25ae1248806c3dfbb563c319
antony-jr <antonyjr@protonmail.com>
2019-06-30 00:07:17 +0530

    use cache dir

--- main.sh
+++ main.sh
@@ -3,20 +3,20 @@
 if [ "$JOB" == "Job1" ]
         then
                 gcc -o job1 job1.c
-                cp job1 "$HOME/.ccache/job1"
+                eval "cp job1 $HOME/dist/job1"
 fi
 
 if [ "$JOB" == "Job2" ]
         then
                 gcc -o job2 job2.c
-                cp job2 "$HOME/.ccache/job2"
+                cp job2 "$HOME/dist/job2"
 fi
 
 if [ "$JOB" == "UseJobs" ]
         then
-                cp "$HOME/.ccache/job1" job1
+                cp "$HOME/dist/job1" job1
                 chmod +x job1
-                cp "$HOME/.ccache/job2" job2
+                cp "$HOME/dist/job2" job2
                 chomd +x job2
 
                 ./job1
