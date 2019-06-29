0780fd1af044495173d3690549e1001028aa4990
antony-jr <antonyjr@protonmail.com>
2019-06-29 23:54:35 +0530

    use cache properly ?

--- main.sh
+++ main.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
 if [ "$JOB" == "Job1" ]
-	then
-		gcc -o job1 job1.c
-		sudo cp job1 "$CACHED_DIR/job1"
+        then
+                gcc -o job1 job1.c
+                cp job1 "$HOME/.ccache/job1"
 fi
 
 if [ "$JOB" == "Job2" ]
-	then
-		gcc -o job2 job2.c
-		sudo cp job2 "$CACHED_DIR/job2"
+        then
+                gcc -o job2 job2.c
+                cp job2 "$HOME/.ccache/job2"
 fi
 
 if [ "$JOB" == "UseJobs" ]
-	then
-		sudo cp "$CACHED_DIR/job1" job1
-		chmod +x job1
-		sudo cp "$CACHED_DIR/job2" job2
-		chomd +x job2
+        then
+                cp "$HOME/.ccache/job1" job1
+                chmod +x job1
+                cp "$HOME/.ccache/job2" job2
+                chomd +x job2
 
-		./job1
-		./job2
+                ./job1
+                ./job2
 fi
+
