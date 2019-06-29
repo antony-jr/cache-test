62461160e28a296a23da262da24328a2bb5caf1c
antony-jr <antonyjr@protonmail.com>
2019-06-29 23:50:21 +0530

    use root

--- main.sh
+++ main.sh
@@ -3,20 +3,20 @@
 if [ "$JOB" == "Job1" ]
 	then
 		gcc -o job1 job1.c
-		cp job1 "$CACHED_DIR/job1"
+		sudo cp job1 "$CACHED_DIR/job1"
 fi
 
 if [ "$JOB" == "Job2" ]
 	then
 		gcc -o job2 job2.c
-		cp job2 "$CACHED_DIR/job2"
+		sudo cp job2 "$CACHED_DIR/job2"
 fi
 
 if [ "$JOB" == "UseJobs" ]
 	then
-		cp "$CACHED_DIR/job1" job1
+		sudo cp "$CACHED_DIR/job1" job1
 		chmod +x job1
-		cp "$CACHED_DIR/job2" job2
+		sudo cp "$CACHED_DIR/job2" job2
 		chomd +x job2
 
 		./job1
